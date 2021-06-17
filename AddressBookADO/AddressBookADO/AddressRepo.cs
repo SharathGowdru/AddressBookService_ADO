53129d1106bfe7ca339b2aee3c688b1a25e2dbbc
Sharath <Sharus9587@gmail.com>
2021-06-17 21:26:26 +0530

    Added : Inserting new contacts to the address book

---
+++ AddressBookADO/AddressBookADO/AddressRepo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookADO
+{
+    class AddressRepo
+    {
+        public static string connectionString = @"Data Source = (localdb)\ProjectsV13;Initial Catalog = Address_book_service; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public bool AddEmployee(AddressModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+
+                    SqlCommand command = new SqlCommand("SpAddPersonDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@FirstName", model.FirstName);
+                    command.Parameters.AddWithValue("@LastName", model.LastName);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@zip", model.zip);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@State", model.State);
+                    command.Parameters.AddWithValue("@Email", model.Email);
+                    command.Parameters.AddWithValue("@AddressBookName", model.AddressBookName);
+                    command.Parameters.AddWithValue("@Type", model.Type);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+            return false;
+        }
+    }
+}
---
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AddressBookADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook");
+            AddressRepo repo = new AddressRepo();
+            AddressModel person = new AddressModel();
+            person.FirstName = "SRI";
+            person.LastName = "RAM";
+            person.Address = "#354 VIdyanagar";
+            person.PhoneNumber = "6565652351";
+            person.zip = 573201;
+            person.City = "Hassan";
+            person.State = "karnataka";
+            person.Email = "Ram@gmail.com";
+            person.AddressBookName = "book4";
+            person.Type = "Family";
+
+            if (repo.AddEmployee(person))
+                Console.WriteLine("Records added successfully");
+            Console.ReadKey();
+        }
+    }
+}
