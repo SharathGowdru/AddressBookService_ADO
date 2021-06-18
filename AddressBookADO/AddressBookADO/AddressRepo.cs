ae1275ab561b47f908809189c8925c312eaf3763
Sharath <Sharus9587@gmail.com>
2021-06-18 05:53:03 +0530

    Added : Getting number of persons belongs to same city

--- AddressBookADO/AddressBookADO/AddressRepo.cs
+++ AddressBookADO/AddressBookADO/AddressRepo.cs
@@ -10,35 +10,23 @@ namespace AddressBookADO
         public static string connectionString = @"Data Source = (localdb)\ProjectsV13;Initial Catalog = Address_book_service; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         SqlConnection connection = new SqlConnection(connectionString);
 
-        public void GetAllEmployee()
+        public void GetSize()
         {
             try
             {
-                AddressModel addressModel = new AddressModel();
+                AddressModel personModel = new AddressModel();
                 using (this.connection)
                 {
-
-                    SqlCommand cmd = new SqlCommand(@"select * from employee_payroll where state='Hassan'", this.connection);
+                    string query = @"select count(city) from employee_payroll group by city;";
+                    SqlCommand cmd = new SqlCommand(query, this.connection);
                     this.connection.Open();
                     SqlDataReader dr = cmd.ExecuteReader();
                     if (dr.HasRows)
                     {
                         while (dr.Read())
                         {
-                            addressModel.FirstName = dr.GetString(0);
-                            addressModel.LastName = dr.GetString(1);
-                            addressModel.Address = dr.GetString(2);
-                            addressModel.PhoneNumber = dr.GetString(6);
-                            addressModel.zip = dr.GetInt32(5);
-                            addressModel.City = dr.GetString(3);
-                            addressModel.State = dr.GetString(4);
-                            addressModel.AddressBookName = dr.GetString(8);
-                            addressModel.Email = dr.GetString(7);
-                            addressModel.Type = dr.GetString(9);
-
-                            System.Console.WriteLine(addressModel.FirstName + " " + addressModel.LastName + " " + addressModel.Address + " " + addressModel.City + " " + addressModel.zip + " " + addressModel.State + " " + addressModel.PhoneNumber + " " + addressModel.AddressBookName + " " + addressModel.Type);
+                            Console.WriteLine(dr.GetInt32(0));
                             System.Console.WriteLine("\n");
-
                         }
                     }
                     else
--- AddressBookADO/AddressBookADO/Program.cs
+++ AddressBookADO/AddressBookADO/Program.cs
@@ -8,19 +8,7 @@ namespace AddressBookADO
         {
             Console.WriteLine("Welcome to AddressBook");
             AddressRepo repo = new AddressRepo();
-            AddressModel person = new AddressModel();
-            person.FirstName = "SRI";
-            person.LastName = "RAM";
-            person.Address = "#354 VIdyanagar";
-            person.PhoneNumber = "6565652351";
-            person.zip = 573201;
-            person.City = "Hassan";
-            person.State = "karnataka";
-            person.Email = "Ram@gmail.com";
-            person.AddressBookName = "book4";
-            person.Type = "Family";
-
-            repo.GetAllEmployee();
+            repo.GetSize();
             Console.ReadKey();
         }
     }
